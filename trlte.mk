fdc0577e1f8b8a3cdc59604daa5f788313641f65
Tony Layher <layhertony@gmail.com>
2014-11-13 22:24:03 -0500

    Build camera and libxml2

--- trlte.mk
+++ trlte.mk
@@ -71,6 +71,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
+# Camera
+PRODUCT_PACKAGES += \
+    camera.apq8084 \
+    libxml2
+
 # Display
 PRODUCT_PACKAGES += \
     copybit.apq8084 \
