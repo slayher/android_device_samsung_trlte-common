97de3aef35453670006e99c6ffb62c57bd2b3642
Tony Layher <layhertony@gmail.com>
2015-01-08 15:13:41 -0500

    trlte-common: Shift to 560dpi for default
    Change-Id: Icc1b8dac777c4fd14bd43cd5f4e3423e1a71e8a2

--- system_prop.mk
+++ system_prop.mk
@@ -12,7 +12,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.hdcp2.rx=tz \
     ro.qualcomm.cabl=1 \
     ro.secwvk=144 \
-    ro.sf.lcd_density=640 \
+    ro.sf.lcd_density=560 \
     ro.opengles.version=196608
 
 # GPS
